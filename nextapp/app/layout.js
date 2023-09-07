9a60858e8fc55f99d26936f0447fc39139afbb75
gi0210lee <gi0210lee@gmail.com>
2023-09-07 16:54:21 +0900

    no 4

--- nextapp/app/layout.js
+++ nextapp/app/layout.js
@@ -8,7 +8,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li>
+            <a href="/read/1">html</a>
+            <a href="/read/2">css</a>
+          </li>
+        </ul>
+        {children}
+        <ul>
+          <li><a href="/create">Create</a></li>
+          <li><a href="/update/1">Update</a></li>
+          <li><input type="button" value="delete"></input></li>          
+        </ul>
+      </body>
     </html>
   )
 }
