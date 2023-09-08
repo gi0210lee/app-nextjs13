6f541680ecb312cca8b60a22287254c0808f8b38
gi0210lee <gi0210lee@gmail.com>
2023-09-08 14:44:47 +0900

    9

--- nextapp/app/api/route.ts
+++ nextapp/app/api/route.ts
@@ -1,5 +1,1 @@
-import { NextResponse } from 'next/server'
- 
-export async function GET(request: Request) {
-  return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
-}
+export async function GET(request: Request) {}
